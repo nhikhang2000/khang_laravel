1e50b8bf1ea9fe93200eed052e70970a21914a72
Tran Nhi Khang <trannhikhang964587@gmail.com>
2021-07-01 09:09:34 +0700

    update

--- 1-clear.bat
+++ 1-clear.bat
@@ -1,5 +1,9 @@
+composer dump-autoload
+composer update
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
 php artisan route:clear
 php artisan storage:link
-php artisan config:clear
-php artisan view:clear
-composer dump-autoload
+php artisan --version
+
